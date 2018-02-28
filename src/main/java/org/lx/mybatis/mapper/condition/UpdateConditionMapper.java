ef170f20b62684ff5ef539064fad26f949d124b9
lanux <lanux@foxmail.com>
2018-02-28 17:42:45 +0800

    init

---
+++ src/main/java/org/lx/mybatis/mapper/condition/UpdateConditionMapper.java
@@ -0,0 +1,9 @@
+package org.lx.mybatis.mapper.condition;
+
+import org.lx.mybatis.annotation.FastMapper;
+import org.lx.mybatis.entity.Condition;
+
+@FastMapper
+public interface UpdateConditionMapper<T> {
+    int updateByCondition(T t, Condition condition);
+}
